c8f3a683e32f37b9bd9ef4a1ac89c70976720cdd
Florian Saqipi <floriansaqipi@gmail.com>
2022-09-16 17:19:26 +0200

    Relocated routes into own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
-  { path: '', redirectTo: '/heroes', pathMatch: 'full'},
-  { path: '**', component: PageNotFoundComponent },
-];
-
 @NgModule({
-  declarations: [AppComponent, CrisisListComponent, HeroListComponent, PageNotFoundComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+  declarations: [
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent,
   ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
